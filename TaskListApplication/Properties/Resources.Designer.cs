50de63fe559841e30f22367fa4802df086c4b138
Lisabessa <75796619+Lisabessa@users.noreply.github.com>
2023-10-25 18:07:49 +0700

    Add form design

--- TaskListApplication/Properties/Resources.Designer.cs
+++ TaskListApplication/Properties/Resources.Designer.cs
@@ -9,7 +9,7 @@
 //------------------------------------------------------------------------------
 
 
-namespace TaskListApp.Properties
+namespace TaskListApplication.Properties
 {
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
@@ -43,7 +43,7 @@ namespace TaskListApp.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TaskListApp.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TaskListApplication.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
